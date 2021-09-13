72ed620d4bd15bb786cd214225ba5ce427909b9f
chadcarlson <chad.carlson@platform.sh>
2021-09-13 16:32:05 -0400

    Files.

--- .platform/updates/upstream.sh
+++ .platform/updates/upstream.sh
@@ -10,8 +10,8 @@ git merge --allow-unrelated-histories -X theirs --squash upstream/master
 rm package-lock.json
 mv README.md README_UPSTREAM.md
 
-# rsync -aP .platform/template/files/ .
-cp .platform/template/files/README.md README.md
+rsync -aP .platform/template/files/ .
+# cp .platform/template/files/README.md README.md
 
 git add .
 STAGED_UPDATES=$(git diff --cached)

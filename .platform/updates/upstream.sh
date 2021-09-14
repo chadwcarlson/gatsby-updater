37d83662752ca4406bc63a6b00f38ced326e7c85
chadcarlson <chad.carlson@platform.sh>
2021-09-14 15:24:09 -0400

    structured feedback.

--- .platform/updates/upstream.sh
+++ .platform/updates/upstream.sh
@@ -3,22 +3,27 @@
 set -e
 
 # Fetch upstream.
+echo "****************** UPDATING TEMPLATE FROM UPSTREAM ****************** "
 git remote add upstream $UPSTREAM_REMOTE              
 git fetch --all
 git merge --allow-unrelated-histories -X theirs --squash upstream/master
+git status
+git add .
 
 # Modify upstream.
+echo "****************** MODIFYING UPSTREAM FOR TEMPLATE ****************** "
 rm package-lock.json
 mv README.md README_UPSTREAM.md
+git status
+git add .
 
 # Add template files.
+echo "****************** UPDATING TEMPLATE FROM UPSTREAM ****************** "
 cp -R .platform/template/files/. .
+git status
+git add .
 
 # Commit.
-git add .
-ls -a
-git status 
-
 STAGED_UPDATES=$(git diff --cached)
 if [ ${#STAGED_UPDATES} -gt 0 ]; then
     git commit -m "Upstream updates."

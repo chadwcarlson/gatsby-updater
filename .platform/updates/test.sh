b7f6f488589468a129fbfb0fb0e336b918be41c1
chadcarlson <chad.carlson@platform.sh>
2021-09-14 14:18:20 -0400

    Commit the thing.

--- .platform/updates/test.sh
+++ .platform/updates/test.sh
@@ -4,3 +4,11 @@ set -e
 
 rsync -aP .platform/template/files/ .
 git add CODE_OF_CONDUCT.md
+
+git add .
+STAGED_UPDATES=$(git diff --cached)
+if [ ${#STAGED_UPDATES} -gt 0 ]; then
+    git commit -m "Upstream updates."
+else
+    echo "No upstream updates found. Skipping."
+fi

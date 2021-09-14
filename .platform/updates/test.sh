e73b4fa2a4738e09bd774017104b96e97b07126a
chadcarlson <chad.carlson@platform.sh>
2021-09-14 14:40:51 -0400

    outside resource.

--- .platform/updates/test.sh
+++ .platform/updates/test.sh
@@ -2,15 +2,21 @@
 
 set -e
 
-git status
-
-rsync -aP .platform/template/files/ .
-git add CODE_OF_CONDUCT.md
+git clone https://github.com/chadwcarlson/common-actions.git
+rsync -aP common-actions .
+ls
 git status
 git add .
 
-git status
-git branch
+# git status
+
+# rsync -aP .platform/template/files/ .
+# git add CODE_OF_CONDUCT.md
+# git status
+# git add .
+
+# git status
+# git branch
 STAGED_UPDATES=$(git diff --cached)
 if [ ${#STAGED_UPDATES} -gt 0 ]; then
     git commit -m "Upstream updates."

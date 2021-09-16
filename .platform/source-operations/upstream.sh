5d5d9f3f367ab63207bff6fc335b33e02b301587
chadcarlson <chad.carlson@platform.sh>
2021-09-16 16:15:53 -0400

    Remove experiment for now.

--- .platform/source-operations/upstream.sh
+++ .platform/source-operations/upstream.sh
@@ -18,26 +18,26 @@ git clone $UPSTREAM upstream-repo
 cp upstream-repo/README.md README_UPSTREAM.md 
 rm -rf upstream-repo
 
-cat package.json | jq -r '.name = "gatsby-starter-blog-platformsh"' \
-            | jq -r '.description = "A Platform.sh starter project for a blog powered by Gatsby and Markdown"' \
-            | jq -r '.author = "Platform.sh"' \
-            | jq -r '.bugs.url = "https://github.com/chadwcarlson/gatsby-updater/issues"' \
-            | jq -r '.homepage = "https://github.com/chadwcarlson/gatsby-updater#readme"' \
-            | jq -r '.repository.url = "git+https://github.com/chadwcarlson/gatsby-updater.git"' \
-            > package.json
+# cat package.json | jq -r '.name = "gatsby-starter-blog-platformsh"' \
+#             | jq -r '.description = "A Platform.sh starter project for a blog powered by Gatsby and Markdown"' \
+#             | jq -r '.author = "Platform.sh"' \
+#             | jq -r '.bugs.url = "https://github.com/chadwcarlson/gatsby-updater/issues"' \
+#             | jq -r '.homepage = "https://github.com/chadwcarlson/gatsby-updater#readme"' \
+#             | jq -r '.repository.url = "git+https://github.com/chadwcarlson/gatsby-updater.git"' \
+#             > package.json
 
-cat package-lock.json | jq -r '.name = "gatsby-starter-blog-platformsh"' \
-            > package-lock.json
+# cat package-lock.json | jq -r '.name = "gatsby-starter-blog-platformsh"' \
+#             > package-lock.json
 
 
-# Update dependencies & verify builds.
-yarn upgrade
-rm -rf node_modules && rm package-lock.json 
-npm install
-npm update
+# # Update dependencies & verify builds.
+# yarn upgrade
+# rm -rf node_modules && rm package-lock.json 
+# npm install
+# npm update
 
-# Format changes.
-yarn format
+# # Format changes.
+# yarn format
 
 # Stage and commit.
 git add .

a78452da79103cbb89b22c08c82d059db7b677e6
chadcarlson <chad.carlson@platform.sh>
2021-09-16 16:23:10 -0400

    CHeck if index works.

--- .platform/source-operations/build-dependencies.sh
+++ .platform/source-operations/build-dependencies.sh
@@ -12,13 +12,13 @@ latest = sys.argv[3]
 if ('v' in sys.argv[3]):
     latest = sys.argv[3][1:]
 
-with open('.platform/applications.yaml', 'r') as file:
+with open('.platform.app.yaml', 'r') as file:
     data = yaml.safe_load(file)
-    current = data[0]['dependencies'][sys.argv[1]][sys.argv[2]]
+    current = data['dependencies'][sys.argv[1]][sys.argv[2]]
     if (latest > current):
-        data[0]['dependencies'][sys.argv[1]][sys.argv[2]] = latest
-        with open('.platform/applications.yaml', 'w') as file:
-            yaml.dump(data, file, indent=4, default_flow_style=True)
+        data['dependencies'][sys.argv[1]][sys.argv[2]] = latest
+        with open('.platform.app.yaml', 'w') as file:
+            yaml.dump(data, file, indent=4, default_flow_style=False)
 END_SCRIPT
 
 python bump.py $1 $2 $3
